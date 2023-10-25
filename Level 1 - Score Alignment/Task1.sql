ddbdaeff7a253eb1cfe26034ebf8093c9b1be195
Soroush Kami <soroushkami10@gmail.com>
2023-10-25 00:24:46 -0400

    task1 - q12595

--- Level 1 - Score Alignment/Task1.sql
+++ Level 1 - Score Alignment/Task1.sql
@@ -61,7 +61,7 @@ WHERE taqr_id = 27171090;
 
 -- correct rows which not 'A' response but got 1.0 score
 UPDATE task1
-SET response = 'A'
+SET score = 0.0
 WHERE question_id = 12563
   AND response != 'A'
   AND score = 1.0;
@@ -71,6 +71,11 @@ WHERE question_id = 12563
 -----------------------------------------
 -- question 12595
 SELECT *
+FROM task1
+WHERE question_id = 12595;
+
+-- check unique score in question_id=12595
+SELECT DISTINCT score
 FROM task1
 WHERE question_id = 12595;
 
@@ -92,18 +97,75 @@ WHERE question_id = 12595;
     full score is 2
 */
 
--- check correct responses that didn't get full score '2'
+-- check correct responses that didn't get full score '2' (English version)
 SELECT *
 FROM task1
 WHERE question_id = 12595
   AND response = 'borrow=>[Receiving money with an agreement to repay it in the future, usually with interest charged.], lend=>[Giving money to a person or a business with the expectation that it will be repaid.], trade=>[The transfer of goods and services, often in exchange for money from one individual or organization to another.]'
-  AND score < 2;
+  AND score != 2.0;
+
+
+-- update rows with correct response but not 2 score (eng version)
+UPDATE task1
+SET score = 2.0
+WHERE question_id = 12595
+  AND response = 'borrow=>[Receiving money with an agreement to repay it in the future, usually with interest charged.], lend=>[Giving money to a person or a business with the expectation that it will be repaid.], trade=>[The transfer of goods and services, often in exchange for money from one individual or organization to another.]'
+  AND score != 2.0;
+
+
+-- check correct responses that didn't get full score '2' (French version)
+SELECT *
+FROM task1
+WHERE question_id = 12595
+  AND response = 'echanges=>[Transférer des biens et des services, souvent en échange d''argent, d''un individu à un autre ou d''une organisation à une autre.], emprunt=>[Recevoir de l''argent en ayant pour accord de le rembourser plus tard, généralement avec des intérêts.], pret=>[Donner de l''argent à une personne ou à une entreprise en s''attendant à ce qu''il soit remboursé.]'
+  AND score != 2.0;
+
+-- update rows with correct response but not 2 score (fr version)
+UPDATE task1
+SET score = 2.0
+WHERE question_id = 12595
+  AND response = 'echanges=>[Transférer des biens et des services, souvent en échange d''argent, d''un individu à un autre ou d''une organisation à une autre.], emprunt=>[Recevoir de l''argent en ayant pour accord de le rembourser plus tard, généralement avec des intérêts.], pret=>[Donner de l''argent à une personne ou à une entreprise en s''attendant à ce qu''il soit remboursé.]'
+  AND score != 2.0;
+
+
+-- no error in this case
+SELECT *
+FROM task1
+WHERE question_id = 12595
+  AND response NOT IN (
+    'borrow=>[Receiving money with an agreement to repay it in the future, usually with interest charged.], lend=>[Giving money to a person or a business with the expectation that it will be repaid.], trade=>[The transfer of goods and services, often in exchange for money from one individual or organization to another.]',
+    'echanges=>[Transférer des biens et des services, souvent en échange d''argent, d''un individu à un autre ou d''une organisation à une autre.], emprunt=>[Recevoir de l''argent en ayant pour accord de le rembourser plus tard, généralement avec des intérêts.], pret=>[Donner de l''argent à une personne ou à une entreprise en s''attendant à ce qu''il soit remboursé.]'
+  )
+  AND score = 2.0;
+
+
+-- SELECT *
+-- FROM task1
+-- WHERE question_id = 12595
+--   AND (
+--     (response NOT LIKE '%borrow=>[Receiving%'
+--     OR response NOT LIKE '%lend=>[Giving%'
+--     OR response NOT LIKE '%trade=>[The transfer%')
+--     AND score != 2
+--   );
+
+-- no errors 
+SELECT *
+FROM task1
+WHERE question_id = 12595
+AND score = 1.33333;
+
+-- no errors here. 
+-- two descriptions are exchanged here which makes the score correct 0.666667
+SELECT *
+FROM task1
+WHERE question_id = 12595
+AND score = 0.666667;
 
 
 
-
-
-
+-----------------------------------------
+-- question 17404
 SELECT *
 FROM task1
 WHERE question_id = 17404;
