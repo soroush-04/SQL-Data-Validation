6808261578d0e8b538049ed6fa0fc0e312966502
Soroush Kami <soroushkami10@gmail.com>
2023-10-25 03:10:27 -0400

    task2 - sql query

--- Level 2 - NR Check/task2.sql
+++ Level 2 - NR Check/task2.sql
@@ -1,1 +1,12 @@
 -- SQLite
+
+SELECT test_question_id FROM task2;
+
+-- learning more about the dataset
+PRAGMA table_info(task2);
+
+SELECT name
+FROM pragma_table_info('task2');
+
+SELECT *
+FROM task2;
